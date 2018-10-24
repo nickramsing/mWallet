e6bbbeb89dbe7c596b04b8ca0d22c48ecd1f403f
nramsing <nramsing@meda.org>
2018-10-24 17:00:20 -0400

    Still testing DB connection updated dbconfig for MEDA site: mwalletdb === SET TO TEST AZURE FIREWALL ===

--- app.py
+++ app.py
@@ -142,8 +142,8 @@ if __name__ == '__main__':
             dbconfig = json.load(json_data_file)
         #dbconfig_environ = dbconfig['DEV']
         #dbconfig_environ = dbconfig['PROD']
-        dbconfig_environ = dbconfig['PRODAzure']
-        #dbconfig_environ = dbconfig['PRODAzure2']
+        #dbconfig_environ = dbconfig['PRODAzure']
+        dbconfig_environ = dbconfig['MEDA']
         if dbconfig_environ['use_azure_appvariable'] == "True":
             env_azure = True    #use the environmental variables from Azure
         else:
